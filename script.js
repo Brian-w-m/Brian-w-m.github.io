2dbc8c25c07bd104c5c3373ff788c709d257497b
Brian-w-m <bwuu0030@student.monash.edu>
2024-01-09 23:05:58 +1100

    radial animation for gridddd

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// For text fly in i think
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -9,8 +11,18 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
-
-
-
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// For radial gradient hover effect
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    }
+}
